473fe574612e200ccfa7112ea8bd719318169d14
Angel Molina <molinao.angel@gmail.com>
2016-01-10 03:41:38 -0500

    login

---
+++ gestidentv1.7/src/java/com/dao/AdminsJpaImp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao;
+
+import com.ec.entidades.Administrador;
+import java.util.List;
+
+/**
+ *
+ * @author ANGEL
+ */
+public class AdminsJpaImp implements AdminDao{
+
+    @Override
+    public List<Administrador> listaAdmin() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+ 
+    
+}
